e583bc2ed13f97e07d6b171ac13847fa4d909f57
Ricardo <ricardo.mook@uni-oldenburg.de>
2022-04-22 11:30:56 +0200

    header erstellt für das spätere benutzen

---
+++ web 2022/header.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+            <li>
+                <a id="nav_start" href="./index.html">Home</a>
+                <a id="suche">Suche</a>
+                <a id="über" href="./about.html">Über uns</a>
+                <a id="nav_anmelden" href="./sign-in.html"> Registrieren/Anmelden</a>
+            </li>
+        </nav>
