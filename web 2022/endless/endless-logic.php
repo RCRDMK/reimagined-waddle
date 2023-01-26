b3484ca1f7220142bbebb377d70fe68b65434bb3
Ricardo Mook <ricardo.mook@uni-oldenburg.de>
2023-01-27 01:18:14 +0900

    .

--- web 2022/endless/endless-logic.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Falls keine Beiträge mehr geladen werden können
-if($_POST["page"]==6){
-    echo "keine Seiten mehr zum laden";
-}
-
-//Aufruf um eine neue Seite zu laden und anzuzeigen
-else{echo "<div class='neue-seite'>PAGE ".$_POST["page"]."</div>";}
-?>
